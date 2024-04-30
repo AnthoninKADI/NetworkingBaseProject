13852d20fa776cd5e8fd6bd510d1960356fc0db9
Anthonin <akadi@artfx.fr>
2024-04-30 10:14:34 +0200

    [Add] Login Window & Name in chat

--- CLIENT/SimpleClient.cpp
+++ CLIENT/SimpleClient.cpp
@@ -3,151 +3,172 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 #include "raylib.h"
-//#include <string>
 using namespace std;
 
 struct Message
 {
-	bool fromMe = false;
-	string content;
-
+    string sender;
+    string content;
 };
 
 int main(int argc, char* argv[])
 {
+    const int width = 400, height = 300;
+    InitWindow(width, height, "Login Window");
+    SetTargetFPS(60);
 
-	const int width = 400, height = 300;
-	InitWindow(width, height, "Login Window");
-	SetTargetFPS(60);
+    string host;
+    string s_port;
+    string name;
+    int port = 4242;
+    bool hostComplete = false;
+    bool nameComplete = false;
+    bool portComplete = false;
 
-	string host;
-	string s_port;
-	string name;
-	int port = 4242;
-	bool ipSelected = false;
-	bool hostComplete = false;
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(GRAY);
-		DrawText("Login", 155, 10, 40, WHITE); // Login Text
-		DrawText("ip", 25, 90, 25, WHITE); // Ip Adress
-		DrawText("port", 10, 160, 25, WHITE); // Port
-		DrawText("name", 10, 230, 25, WHITE); // Name
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        DrawText("Login", 155, 10, 40, WHITE); // Login Text
+        DrawText("ip", 25, 90, 25, WHITE);    // Ip Adress
+        DrawText("port", 10, 160, 25, WHITE); // Port
+        DrawText("name", 10, 230, 25, WHITE); // Name
 
-		DrawRectangle(70, 90, width - 100, 25, DARKGRAY);
-		DrawRectangle(70, 160, width - 100, 25, DARKGRAY);
-		DrawRectangle(70, 230, width - 100, 25, DARKGRAY);
+        DrawRectangle(70, 90, width - 100, 25, WHITE);
+        DrawRectangle(70, 160, width - 100, 25, WHITE);
+        DrawRectangle(70, 230, width - 100, 25, WHITE);
 
-		int inputChar = GetCharPressed();
+        int inputChar = GetCharPressed();
 
-		if (inputChar != 0)
-		{
-			if (!hostComplete) host += static_cast<char>(inputChar);
-			else s_port += static_cast<char>(inputChar);
+        if (inputChar != 0)
+        {
+            if (!hostComplete)
+            {
+                host += static_cast<char>(inputChar);
+            }
+            else if (!portComplete)
+            {
+                s_port += static_cast<char>(inputChar);
+            }
+            else if (!nameComplete)
+            {
+                name += static_cast<char>(inputChar);
+            }
+        }
+        if (IsKeyPressed(KEY_ENTER))
+        {
+            if (!hostComplete)
+            {
+                hostComplete = true;
+            }
+            else if (!portComplete)
+            {
+                port = stoi(s_port);
+                portComplete = true;
+            }
+            else if (!name.empty())
+            {
+                nameComplete = true;
+                break; // Quitte la boucle une fois que le nom est entré
+            }
+        }
+        else if (IsKeyPressed(KEY_BACKSPACE))
+        {
+            if (hostComplete && !name.empty())
+                name.pop_back();
+            else if (portComplete && !s_port.empty())
+                s_port.pop_back();
+            else if (!host.empty())
+                host.pop_back();
+        }
+        if (!host.empty())
+        {
+            DrawText(host.c_str(), 75, 90, 25, BLACK);
+        }
+        if (!s_port.empty())
+        {
+            DrawText(s_port.c_str(), 75, 160, 25, BLACK);
+        }
+        if (!name.empty())
+        {
+            DrawText(name.c_str(), 75, 230, 25, BLACK);
+        }
+        EndDrawing();
+    }
+    CloseWindow();
 
-		}
-		if (IsKeyPressed(KEY_ENTER))
-		{
-			if (!hostComplete)
-			{
-				hostComplete = true;
-				cout << hostComplete << endl;
-			}
-			else
-			{
-				port = stoi(s_port);
-				break;
-			}
-		}
-		else if (IsKeyPressed(KEY_BACKSPACE))
-		{
-			if (hostComplete && !s_port.empty()) s_port.pop_back();
-			else if (!host.empty())  host.pop_back();
-		}
-		if (!host.empty())
-		{
-			DrawText(host.c_str(), 75, 90, 25, WHITE);
-		}
-		if (!s_port.empty())
-		{
-			DrawText(s_port.c_str(), 75, 160, 25, WHITE);
-		}
+    if (SDLNet_Init() == -1)
+    {
+        cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
+        return 1;
+    }
 
-		EndDrawing();
+    IPaddress ip;
 
-	}
-	CloseWindow();
+    if (SDLNet_ResolveHost(&ip, host.c_str(), port) == -1)
+    {
+        cerr << "Resolver Host error: " << SDLNet_GetError() << endl;
+        return 1;
+    }
 
-	if (SDLNet_Init() == -1) {
-		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
-		return 1;
-	}
+    TCPsocket clientSocket = SDLNet_TCP_Open(&ip);
+    if (!clientSocket)
+    {
+        cerr << "TCP Open error: " << SDLNet_GetError() << endl;
+        SDLNet_Quit();
+        return 1;
+    }
 
-	IPaddress ip;
+    string typing;
+    const int Mwidth = 500, Mheight = 750;
+    InitWindow(Mwidth, Mheight, "ChatBox Window");
+    SetTargetFPS(60);
 
+    vector<Message> log{Message{"Welcome to the ChatBox"}};
 
-	if (SDLNet_ResolveHost(&ip, host.c_str(), port) == -1)
-	{
-		cerr << "Resolver Host error: " << SDLNet_GetError() << endl;
-		return 1;
-	}
+    if (!typing.empty())
+        DrawText("Welcome to ChatBox", 150, 15, 25, SKYBLUE);
+    else
+        DrawText("Welcome to ChatBox", 150, 15, 25, WHITE);
 
-	TCPsocket clientSocket = SDLNet_TCP_Open(&ip);
-	if (!clientSocket)
-	{
-		cerr << "TCP Open error: " << SDLNet_GetError() << endl;
-		SDLNet_Quit();
-		return 1;
-	}
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(GRAY);
+        DrawText("Welcome to ChatBox", 150, 15, 25, WHITE);
+        DrawRectangle(20, 50, Mwidth - 40, Mheight - 150, DARKGRAY);
+        DrawRectangle(20, Mheight - 90, Mwidth - 40, 50, WHITE);
 
-	string typing;
-	const int Mwidth = 500, Mheight = 750;
-	InitWindow(Mwidth, Mheight, "ChatBox Window");
-	SetTargetFPS(60);
+        int inputChar = GetCharPressed();
+        if (inputChar != 0)
+        {
+            typing += static_cast<char>(inputChar);
+        }
+        if (!typing.empty())
+        {
+            if (IsKeyPressed(KEY_BACKSPACE))
+                typing.pop_back();
+            else if (IsKeyPressed(KEY_ENTER))
+            {
+                log.push_back(Message{name, typing});
+                typing.clear();
+            }
+            DrawText(typing.c_str(), 30, Mheight - 75, 25, BLACK);
+        }
 
-	vector<Message> log{Message{false, "Welcome to the ChatBox"}};
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(GRAY);
-		DrawText("Welcome to ChatBox", 150, 15, 25, WHITE);
-		DrawRectangle(20, 50, Mwidth - 40, Mheight - 150, DARKGRAY);
-		DrawRectangle(20, Mheight - 90, Mwidth - 40, 50, LIGHTGRAY);
+        for (size_t i = 0; i < log.size(); i++)
+        {
+            DrawText(TextFormat("[%s] %s", log[i].sender.c_str(), log[i].content.c_str()), 30, 75 + (i * 30), 15, SKYBLUE);
 
-		int inputChar = GetCharPressed();
-		if (inputChar != 0)
-		{
-			typing += static_cast<char>(inputChar);
-		}
-		if (!typing.empty())
-		{
-			if (IsKeyPressed(KEY_BACKSPACE)) typing.pop_back();
-			else if (IsKeyPressed(KEY_ENTER))
-			{
-				log.push_back(Message{ true,typing });
-				typing.clear();
-			}
-			DrawText(typing.c_str(), 30, Mheight - 75, 25, DARKBLUE);
-		}
+        }
 
-		for (int msg = 0; msg < log.size(); msg++)
-		{
-			DrawText(log[msg].content.c_str(), 30, 75 + (msg * 30), 15, log[msg].fromMe ? SKYBLUE : PURPLE);
-		}
-
-		EndDrawing();
-
-	}
-	CloseWindow();
-
-
+        EndDrawing();
+    }
+    CloseWindow();
 
 	string message;
-
-	cout << "Please enter a message : " << endl;
+	cout << "Send a message : " << endl;
 	cin.clear();
 	getline(cin, message);
 
